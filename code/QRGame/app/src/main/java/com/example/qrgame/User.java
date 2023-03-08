3914db46322fed4cc3af74c4005e4fb42c6be3c1
Zheng Pang <zpang1@ualberta.ca>
2023-03-07 23:10:23 -0700

    update device unique ID

--- code/QRGame/app/src/main/java/com/example/qrgame/User.java
+++ code/QRGame/app/src/main/java/com/example/qrgame/User.java
@@ -1,12 +1,15 @@
 package com.example.qrgame;
 
+
 public class User {
     private String username;
     private String phonenumber;
+    private String androidId;
 
-    public User(String username, String phonenumber) {
+    public User(String username, String phonenumber,String androidId) {
         this.username = username;
         this.phonenumber = phonenumber;
+        this.androidId = androidId;
     }
 
     public String getUsername() {
@@ -17,5 +20,7 @@ public class User {
         return phonenumber;
     }
 
-
+    public String getAndroidId() {
+        return androidId;
+    }
 }
