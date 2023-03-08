3c089b0e134207b1924f7f612f02ca0d33f0f0f3
Zheng Pang <zpang1@ualberta.ca>
2023-03-07 23:11:22 -0700

    add device ID to firebase

--- code/QRGame/app/src/main/java/com/example/qrgame/SignUp.java
+++ code/QRGame/app/src/main/java/com/example/qrgame/SignUp.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -25,6 +27,7 @@ import org.checkerframework.checker.units.qual.A;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -34,58 +37,64 @@ import androidx.fragment.app.Fragment;
 public class SignUp extends AppCompatActivity {
     private Button Finish;
     ArrayList<User> dataList;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
         Button Finish = findViewById(R.id.Finish_button);
-        EditText UserName=findViewById(R.id.Username);
-        EditText PhoneNumber=findViewById(R.id.PhoneNumber);
+        EditText UserName = findViewById(R.id.Username);
+        EditText PhoneNumber = findViewById(R.id.PhoneNumber);
 
         //firebase initialize
         FirebaseApp.initializeApp(this);
-        FirebaseFirestore fireStore=FirebaseFirestore.getInstance();
+        FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
 
         //store the data
-        Map<String,Object> curUser=new HashMap<>();
-        dataList=new ArrayList<>();
+        Map<String, Object> curUser = new HashMap<>();
+        dataList = new ArrayList<>();
         Intent sign_page = new Intent(this, MainPageActivity.class);
         //click Finish bottom to save data to firebase
         Finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userName=UserName.getText().toString();
-                String phoneNumber=PhoneNumber.getText().toString();
+                String userName = UserName.getText().toString();
+                String phoneNumber = PhoneNumber.getText().toString();
+                String androidId = getUdid();
 
-
-                if (userName.isEmpty()){
-                    Toast warningToast=Toast.makeText(SignUp.this,"Username is empty",Toast.LENGTH_LONG);
+                if (userName.isEmpty()) {
+                    Toast warningToast = Toast.makeText(SignUp.this, "Username is empty", Toast.LENGTH_LONG);
                     warningToast.show();
-                }else if(phoneNumber.isEmpty()){
-                    Toast warningToast=Toast.makeText(SignUp.this,"PhoneNumber is empty",Toast.LENGTH_LONG);
+                } else if (phoneNumber.isEmpty()) {
+                    Toast warningToast = Toast.makeText(SignUp.this, "PhoneNumber is empty", Toast.LENGTH_LONG);
                     warningToast.show();
-                }else {
-                    dataList.add((new User(userName,phoneNumber)));
+                } else {
+                    dataList.add((new User(userName, phoneNumber, androidId)));
+                    CollectionReference user = fireStore.collection("UserCollection");
                     curUser.put("UserNameKey", dataList.get(0).getUsername());
                     curUser.put("PhoneKey", dataList.get(0).getPhonenumber());
+                    curUser.put("AndroidKey", dataList.get(0).getAndroidId());
 
-                    fireStore.collection("UserCollection")
-                            .add(curUser)
+                    user.document(dataList.get(0).getUsername()).set(curUser);
+                    /**
+                     fireStore.collection("UserCollection")
 
-                            .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentReference> task) {
-                                    if (task.isSuccessful()) {
-                                        Log.d("RRG", "Document is success");
-                                    } else {
-                                        Log.e("RRG", "something wrong", task.getException());
-                                    }
-                                }
+                     .add(curUser)
 
-                            });
+                     .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                    if (task.isSuccessful()) {
+                    Log.d("RRG", "Document is success");
+                    } else {
+                    Log.e("RRG", "something wrong", task.getException());
+                    }
+                    }
 
+                    });
 
+                     **/
                     startActivity(sign_page);
 
                 }
@@ -95,5 +104,15 @@ public class SignUp extends AppCompatActivity {
         });
 
 
+    }
+
+    public String AndroidID() {
+        String id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        return id == null ? "" : id;
+    }
+
+    public String getUdid() {
+        String androidID=AndroidID();
+        return"2"+ UUID.nameUUIDFromBytes(androidID.getBytes()).toString().replace("-","");
     }
 }
