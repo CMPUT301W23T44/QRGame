461e2dc17d6a921f8a377ce75204d42f8aa8c617
Jacky Zhao <Zhaotianqi2@gmail.com>
2023-03-10 23:26:37 -0700

    Revert "Merge branch 'main' of https://github.com/CMPUT301W23T44/QRGame into JackyZhao"
    This reverts commit 1b258034186c1dbbe8b6bf1cd4209ccc9a308f48, reversing
    changes made to b630c2a94fd238d5e167873f3540b00806a3e04b.

--- code/QRGame/app/src/main/java/com/example/qrgame/QRFirebase.java
+++ code/QRGame/app/src/main/java/com/example/qrgame/QRFirebase.java
@@ -1,101 +1,21 @@
 package com.example.qrgame;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * Interface between the program app and the online Firebase database
- */
 public class QRFirebase {
 
+   private static FirebaseFirestore qrDB;
 
-    private static FirebaseFirestore qrDB = FirebaseFirestore.getInstance();
-    private static String COLLECTION_NAME = "qrCodes";
-    private final static CollectionReference QR_CODE_COLLECTION = qrDB.collection(COLLECTION_NAME);
+   public static void addQR(QRCode qrCode) {
 
-    private final static String TAG = "QR_adder";
+   }
 
-    /**
-     * Adds a QR code to the Firebase database
-     * @param qrCode - QR code to be added to the database
-     */
-    public static void addQR(QRCode qrCode) {
-        if (!findQR(qrCode)) {
-            QR_CODE_COLLECTION
-                    .document(qrCode.getHash())
-                    .set(qrCode)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Log.d(TAG, "Add successful");
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "Add failure");
-                        }
-                    });
-        }
-    }
+   public static void deleteQR(QRCode qrCode) {
 
-    /**
-     * Deletes a provided QR code from the database, if it exists
-     * @param qrCode - QR code to be deleted
-     */
-    public static void deleteQR(QRCode qrCode) {
-        if (findQR(qrCode)) {
-            QR_CODE_COLLECTION.document(COLLECTION_NAME)
-                    .delete()
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Log.d(TAG, "Delete successful");
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "Delete failure");
-                        }
-                    });
-        }
-    }
+   }
 
-    /**
-     * Finds a provided QR code in the database
-     * @param qrCode - QR code to be found
-     * @return
-     *      True if the QR code exists in the database
-     */
-    public static boolean findQR(QRCode qrCode) {
-        final boolean[] result = new boolean[1];
-        DocumentReference docRef = qrDB.collection(COLLECTION_NAME).document(qrCode.getHash());
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if (documentSnapshot.exists()) {
-                        result[0] = true;
-                    } else {
-                        result[0] = false;
-                    }
-                } else {
-                    Log.d(TAG, "Get failure");
-                }
-            }
-        });
-        return result[0];
-    }
+   public static boolean findQR(String hash) {
+      return false;
+   }
+
 }
