1be419d8844df5073531ec1a4a67d757598b6987
Jacky Zhao <Zhaotianqi2@gmail.com>
2023-03-23 16:28:28 -0600

    Revert "Merge branch 'JackyZhao' of https://github.com/CMPUT301W23T44/QRGame into JackyZhao"
    This reverts commit b5d11b696de564f1a540ab499e5beb5d8a781da2, reversing
    changes made to 6f61e9ee120a82e9f76141a4fdf86f390231878b.

--- code/QRGame/app/src/main/java/com/example/qrgame/Map.java
+++ code/QRGame/app/src/main/java/com/example/qrgame/Map.java
@@ -36,55 +36,121 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class Map extends DialogFragment {
     FirebaseFirestore firebaseDatabase;
     private String value;
+    private Button search;
 
+    private GeoPoint loc;
+//    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+
+    @SuppressLint("MissingInflatedId")
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        firebaseDatabase = FirebaseFirestore.getInstance();
+        CollectionReference test_value = firebaseDatabase.collection("qrCodes");
 
 
 
         View view = LayoutInflater.from(getContext()).inflate(R.layout.map, null);
         EditText la = view.findViewById(R.id.latitude);
         EditText lo = view.findViewById(R.id.longitude);
+        search = view.findViewById(R.id.search);
+        if(search != null) {
+            search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String lat = la.getText().toString();
+                    String lot = lo.getText().toString();
+                    GeoPoint loc1 = new GeoPoint(Double.parseDouble(lat), Double.parseDouble(lot));
+                    Toast.makeText(Map.this.getContext(), loc1.toString(), Toast.LENGTH_SHORT).show();
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        firebaseDatabase = FirebaseFirestore.getInstance();
-        CollectionReference test_value = firebaseDatabase.collection("qrCodes");
-        test_value
+                    //AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    test_value
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @SuppressLint("RestrictedApi")
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @SuppressLint("RestrictedApi")
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        for (DocumentSnapshot document : task.getResult().getDocuments()) {
+                                            //value = String.valueOf(task.getResult().getDocuments().get(0).getString("name"));
 
-                        if (task.isSuccessful()) {
-                            for (DocumentSnapshot document : task.getResult().getDocuments()) {
-                                //value = String.valueOf(task.getResult().getDocuments().get(0).getString("name"));
+                                            value = document.getString("name");
+                                            value += " location: ";
+//                                        GeoPoint loc1 = new GeoPoint(Double.parseDouble(lat), Double.parseDouble(lot));
+                                            if (document.getGeoPoint("location_test") == loc1) {
+                                                Toast.makeText(Map.this.getContext(), loc1.toString(), Toast.LENGTH_SHORT).show();
 
-                                value = document.getString("name");
-                                value += " location: ";
-                                value += document.getString("location_test_string");
-                                Toast.makeText(Map.this.getContext(), value, Toast.LENGTH_SHORT).show();
-                            }
+
+                                            }
+                                            value += document.getString("location_test_string");
+                                            Toast.makeText(Map.this.getContext(), value, Toast.LENGTH_SHORT).show();
+                                        }
 //                            Intent intent = new Intent(Map.this.getContext(), Activity.class);
 //                            intent.putExtra("name",task.getResult().getDocuments().get(0).getString("name"));
 //                            startActivity(intent);
 
 
-                        } else {
+                                    } else {
 //                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-//        Intent intent=Intent.parseUri();
-//        String name=intent.getStringExtra("name");
+                                    }
+                                }
+//                            });
+
+
+                            });
+                }
+            });
+        }
+//        //AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+//        //firebaseDatabase = FirebaseFirestore.getInstance();
+//        //CollectionReference test_value = firebaseDatabase.collection("qrCodes");
+//        test_value
+//
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @SuppressLint("RestrictedApi")
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//
+//                        if (task.isSuccessful()) {
+//                            for (DocumentSnapshot document : task.getResult().getDocuments()) {
+//                                //value = String.valueOf(task.getResult().getDocuments().get(0).getString("name"));
+//
+//                                value = document.getString("name");
+//                                value += " location: ";
+//                                String lat = la.getText().toString();
+//                                String lot = lo.getText().toString();
+////                                GeoPoint loc1 = new GeoPoint(Double.parseDouble(lat),Double.parseDouble(lot));
+////                                if (document.getGeoPoint("location_test") == loc1){
+////                                    Toast.makeText(Map.this.getContext(), loc1.toString(), Toast.LENGTH_SHORT).show();
+////
+////
+////                                }
+//                                value += document.getGeoPoint("location_test");
+//                                Toast.makeText(Map.this.getContext(), value, Toast.LENGTH_SHORT).show();
+//                            }
+////                            Intent intent = new Intent(Map.this.getContext(), Activity.class);
+////                            intent.putExtra("name",task.getResult().getDocuments().get(0).getString("name"));
+////                            startActivity(intent);
+//
+//
+//                        } else {
+////                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+////        Intent intent=Intent.parseUri();
+////        String name=intent.getStringExtra("name");
 
 
 
@@ -92,6 +158,51 @@ public class Map extends DialogFragment {
                 .setView(view)
                 .setTitle("test" + value)
                 .setNegativeButton("Cancel", null)
+                .setPositiveButton("Search", (dialog, which) -> {
+                    String lat = la.getText().toString();
+                    String lot = lo.getText().toString();
+                    GeoPoint loc1 = new GeoPoint(Double.parseDouble(lat), Double.parseDouble(lot));
+                    Toast.makeText(Map.this.getContext(), loc1.toString(), Toast.LENGTH_SHORT).show();
+
+                    //AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    test_value
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @SuppressLint("RestrictedApi")
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                    if (task.isSuccessful()) {
+                                        for (DocumentSnapshot document : task.getResult().getDocuments()) {
+                                            //value = String.valueOf(task.getResult().getDocuments().get(0).getString("name"));
+
+                                            value = document.getString("name");
+                                            value += " location: ";
+//                                        GeoPoint loc1 = new GeoPoint(Double.parseDouble(lat), Double.parseDouble(lot));
+                                            if (document.getGeoPoint("location_test") == loc1) {
+                                                Toast.makeText(Map.this.getContext(), loc1.toString(), Toast.LENGTH_SHORT).show();
+
+
+                                            }
+                                            //value += document.getString("location_test_string");
+                                            //Toast.makeText(Map.this.getContext(), value, Toast.LENGTH_SHORT).show();
+                                        }
+//                            Intent intent = new Intent(Map.this.getContext(), Activity.class);
+//                            intent.putExtra("name",task.getResult().getDocuments().get(0).getString("name"));
+//                            startActivity(intent);
+
+
+                                    } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+                                    }
+                                }
+//                            });
+
+
+                            });
+
+                })
                 .create();
+//        return null;
     }
 }
